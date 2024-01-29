a91ed79f38f22da0e03965928c5d25f49150d916
John Arban Lewis <johnarban@users.noreply.github.com>
2024-01-29 11:46:11 -0500

    fix text to reflect current state

--- src/SolarEclipse2024.vue
+++ src/SolarEclipse2024.vue
@@ -89,8 +89,8 @@
               <span class="description">
                 <div class=".d-flex">
                   <div>
-                The map shows an overlay of the expected cloud coverage on April 8 based on historical cloud cover data for April 8 from 2001 - 2023 from the <a href="https://modis.gsfc.nasa.gov/">NASA MODIS</a> Aqua satellite.
-                It shows the median cloud coverage for each position. In other words, 50% of days had more, and 50% of days had less clouds. 
+                Clicking on the map gives the expected cloud coverage on April 8 based on historical cloud cover data for April 8 from 2001 - 2023 from the <a href="https://modis.gsfc.nasa.gov/">NASA MODIS</a> Aqua satellite.
+                It shows the median cloud coverage for each position (within 100 km). In other words, 50% of days had more, and 50% of days had less clouds. 
                   </div>
                   <div>
                     <cloud-cover
