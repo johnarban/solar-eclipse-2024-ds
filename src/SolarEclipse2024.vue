70ece13f7d840e392d393cc1d3bc130b8ecdb6b4
John Arban Lewis <johnarban@users.noreply.github.com>
2024-02-09 10:02:08 -0500

    fix sliver of blue moon left during totality

--- src/SolarEclipse2024.vue
+++ src/SolarEclipse2024.vue
@@ -1546,7 +1546,7 @@ export default defineComponent({
       uuid,
       responseOptOut: responseOptOut as boolean | null,
 
-      showSplashScreen: true,
+      showSplashScreen: false,
       backgroundImagesets: [] as BackgroundImageset[],
       sheet: null as SheetType,
       layersLoaded: false,
@@ -2440,7 +2440,7 @@ export default defineComponent({
       
       let forceTotality = false;
       if (this.locationInTotality && this.inEclipse) {
-        if (this.currentFractionEclipsed < 1) {
+        if (this.currentFractionEclipsed <= 1) {
           console.log('forcing', this.toTimeString(this.wwtCurrentTime, true, false), this.currentFractionEclipsed);
           this.currentFractionEclipsed = 1;
           forceTotality = true;
@@ -2455,15 +2455,16 @@ export default defineComponent({
       }
 
       const n = 50;
-      
+      console.table({'forceTotality': forceTotality,  'currentFractionEclipsed': this.currentFractionEclipsed});
       // If the moon/sun is completely "inside" of the sun/moon
       if (moonInsideSun || sunInsideMoon || forceTotality) {
+        console.log('using circle');
         for (let i = 0; i <= n; i++) {
           const angle = (i / n) * 2 * Math.PI;
           points.push({ x: rMoonPx * Math.cos(angle), y: rMoonPx * Math.sin(angle) });
         }
       } else {
-
+        console.log('finding lens');
         let x1: number;
         let y1: number;
         let x2: number;
