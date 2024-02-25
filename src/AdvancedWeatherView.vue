ff74c9e7187d6372755b742dd6415a5ed0bd4ab1
John Arban Lewis <johnarban@users.noreply.github.com>
2024-02-24 20:20:38 -0500

    update layout of AWV

--- src/AdvancedWeatherView.vue
+++ src/AdvancedWeatherView.vue
@@ -33,19 +33,75 @@
           
           <v-col cols="6">
             <v-row>
-              <v-col cols="12" md="6">
+              <v-col cols="12">
                 <v-select
+                  density="compact"
                   v-model="selectedYear"
-                  :items="selectedYearRange"
-                  label="Year"
+                  :items="availableYears"
+                  label="Select a Single Year to Veiw"
+                  hide-details
                 ></v-select>
+                <v-slider
+                  class="thumb-label-below"
+                  :model-value="selectedYear"
+                  @end="selectedYear = $event"
+                  :min="availableYears[0]"
+                  :max="availableYears[availableYears.length - 1]"
+                  :step="1"
+                  show-ticks="always"
+                  
+                  hide-details
+                  thumb-label="always"
+                  ></v-slider>
               </v-col>
-              <v-col cols="12" md="6">
-                <v-select
-                  v-model="selectedStat"
-                  :items="['mean', 'max', 'min']"
-                  label="Statistic"
-                ></v-select>
+              <v-col cols="12">
+                <v-col>
+                  <p> Select a statistic to view</p>
+                <div class="d-flex flex-wrap justify-space-around" style="gap:5px;">
+                  <v-btn
+                  v-for="stat in (['mean', 'median', 'max', 'min'] as Statistics[])" :key="stat"
+                    v-model="selectedStat"
+                    :value="stat"
+                    rounded="lg"
+                    :variant="selectedStat == stat ? 'tonal' : 'outlined'"
+                    @click="selectedStat = stat"
+                    >
+                    {{ stat }}
+                  </v-btn>
+                </div>
+                </v-col>
+                <v-col>
+                  <p> 
+                    2024 is an El Nino year. 
+                    El Nino affects broad weather patterns, 
+                    including cloud cover. <br />
+                    Show me: </p>
+                    <div class="d-flex flex-wrap justify-space-around" style="gap:5px;">
+                      <v-btn
+                      v-for="preference in (['elNino', 'noElNino', 'allYears'] as ElNinoPreference[])" :key="preference"
+                        v-model="elNinoPreference"
+                        :value="preference"
+                        rounded="lg"
+                        :variant="elNinoPreference == preference ? 'tonal' : 'outlined'"
+                        @click="elNinoPreference = preference"
+                        >
+                        {{ preference }}
+                      </v-btn>
+                    </div>
+                  <!-- <v-btn-toggle
+                    style="gap:2px"
+                    v-model="elNinoPreference"
+                    density="compact"
+                    divided
+                    rounded="xl"
+                    mandatory
+                    variant="flat"
+                    >
+                    <v-btn value="elNino">El Nino</v-btn>
+                    <v-btn value="noElNino">No El Nino</v-btn>
+                    <v-btn value="allYears">All Years</v-btn>
+                  </v-btn-toggle> -->
+                </v-col>
               </v-col>
             </v-row>
           </v-col>
@@ -69,7 +125,7 @@
         
         <!-- j -->
         <v-row class="bg-red-lighten-3">
-          <v-col cols="6" class="graph-col">
+          <v-col cols="12" sm="6" class="graph-col">
           <bar-chart
             id="cloud-histogram"
             class="elevation-5"
@@ -79,10 +135,13 @@
             :title="`Cloud Conditions for ${selectedYear}`"
             />
           </v-col>
-          <v-col cols="6" class="graph-col">
+          <v-col cols="12" sm="6" class="graph-col">
           <line-chart
             class="elevation-5"
             :scatter-data="scatterData"
+            :line-data="lineData"
+            timeseries
+            line-color="blue"
             show-line
             show-scatter
             />
@@ -103,6 +162,15 @@ import { defineComponent, PropType } from 'vue';
 import BarChart from './BarChart.vue';
 import LineChart from './LineChart.vue';
 import LocationSelector from './LocationSelector.vue';
+import {generateFakeTimeSeries} from './utils';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import {isNumber, toOrderedPairs, fromOrderedPairs, elementWise, OrderedPair} from './utils';
+// isNumber is a utility function that checks if a value is a number
+// toOrderedPairs converts an array of objects to an array of ordered pairs
+// fromOrderedPairs converts an array of {x:x[i],x:y[i]}[] ordered pairs to an array of [x,y]
+// elementWise applies a function to each element of an array returning an ordered pair
+// We need to use Ordered Pairs as that is the data format that Chart.js expects
+type LineGraphData = OrderedPair<number | Date, number>[];
 
 type CityLocation = {
   longitudeDeg: number;
@@ -115,27 +183,31 @@ const cityBoston: CityLocation = {
 };
 
 function randomData(n: number): number[] {
+  // for testing purposes only
   const data = Array.from({ length: n }, () => Math.round((Math.random() * 100)));
   const sum = data.reduce((a, b) => a + b, 0);
   return data.map((d) => Math.round((d / sum) * 140));
 }
 
-function randomArray(n: number) {
-  // create an array of n objects with x and y properties
-  return Array.from({ length: n }, () => (
-    { 
-      x: Math.random() * 100, 
-      y: Math.random() * 100 
-    }
-  ));
-}
+
+type Statistics = 'mean' | 'median' | 'max' | 'min';
+type ElNinoPreference = 'elNino' | 'noElNino' | 'allYears';
 
 
-
-type ScatterData = { x: number;y: number;}[];
 
 // https://colorbrewer2.org/#type=sequential&scheme=YlGnBu&n=6
 const _colorMap = ['#ffffcc','#c7e9b4','#7fcdbb','#41b6c4','#2c7fb8','#253494'];
+
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+// const LocationSelector = defineAsyncComponent({
+//   loader: () => import('./LocationSelector.vue'),
+//   loadingComponent: defineComponent({
+//     template: '<v-progress-circular indeterminate color="primary"></v-progress-circular>',
+//   }),
+//   delay: 10,
+//   timeout: 3000,
+// });
 
 export default defineComponent({
   name: 'AdvancedWeatherView',
@@ -166,11 +238,12 @@ export default defineComponent({
     return {
       location: this.defaultLocation,
       dataLoadingProgress: 0,
+      allYears: Array.from({ length: 22 }, (_, i) => 2001 + i),
       selectedYear: 2021,
       selectedYearRange: [2001, 2022],
-      selectedStat: 'mean',
+      selectedStat: 'mean' as Statistics,
+      elNinoPreference: 'allYears' as ElNinoPreference,
       cloudDataHistogram: randomData(6), // overcast, mostlyCloudy, partlyCloudy, fewClouds, clear
-      scatterData: randomArray(100) as ScatterData,
       // https://www.weather.gov/media/notification/dir/AFM_Specifications.pdf
       skyCoverCodes: ['Clear', 'Mostly Clear', 'Parly Cloudy', 'Mostly Cloudy', 'Considerably Cloudy', 'Overcast'],
       skyCoverCodeRanges: {
@@ -198,6 +271,12 @@ export default defineComponent({
         fillOpacity: 0.7,
         radius: 5 
       },
+      
+      // some fake data for the plot
+      // scatterData: evaluate(0, 2 * Math.PI, 20, noisySin) as LineGraphData,
+      // lineData: evaluate(0, 2 * Math.PI, 100, sin) as LineGraphData,
+      scatterData: generateFakeTimeSeries(new Date(2021, 0, 1), new Date(2021, 11, 31), 20, 1) as LineGraphData,
+      lineData: generateFakeTimeSeries(new Date(2021, 0, 1), new Date(2021, 11, 31), 100, 0) as LineGraphData,
     };
   },
   
@@ -211,19 +290,19 @@ export default defineComponent({
         console.log('AdvancedWeatherView showValue set to', value);
         this.$emit('update:modelValue', value);
       },
-    }
+    },
+    
+    availableYears() {
+      // conceivably there could be years that are not available
+      // we could handle that here. Otherwise this is just a passthrough
+      return this.allYears;
+    },
     
   },
   
   mounted() {
     console.log('Advanced Weather View mounted');
     // create a time to simulate data loading
-    const interval = setInterval(() => {
-      this.dataLoadingProgress += 10;
-      if (this.dataLoadingProgress >= 100) {
-        clearInterval(interval);
-      }
-    }, 1000);
   },
   
   methods: {
@@ -232,8 +311,29 @@ export default defineComponent({
     },
     
     randomData() {
+      // just for testing
       this.cloudDataHistogram = randomData(6);
-      this.scatterData = randomArray(100) as ScatterData;
+      // this.scatterData = evaluate(0, 2 * Math.PI, 20, noisySin);
+      // this.lineData = evaluate(0, 2 * Math.PI, 100, sin);
+      // generate some random timeseries data
+      const start = new Date(2021, 0, 1);
+      const end = new Date(2021, 11, 31);
+      this.scatterData = generateFakeTimeSeries(start, end, 20, 1);
+      this.lineData = generateFakeTimeSeries(start, end, 100, 0);
+    },
+    
+    // stats methods
+    mean(array: number[], ignoreNaN: boolean = true): number {
+      // null + x, undefined +x = NaN. 
+      const arr = ignoreNaN ? array.filter((v) => isNumber(v)) : array;
+      return arr.reduce((a, b) => a + b, 0) / arr.length;
+    },
+    
+    median(array: number[], ignoreNaN: boolean = true): number {
+      const arr = ignoreNaN ? array.filter((v) => isNumber(v)) : array;
+      const mid = Math.floor(arr.length / 2);
+      const nums = [...arr].sort((a, b) => a - b);
+      return arr.length % 2 == 0 ? (nums[mid] + nums[mid - 1]) / 2 : nums[mid];
     }
   },
   
@@ -259,6 +359,21 @@ export default defineComponent({
     aspect-ratio: 1;
   }
 
+  div {
+    outline: 0px solid red;
+  }
+  
+  .thumb-label-below .v-slider-thumb__label {
+    --pos: calc(var(--v-slider-thumb-size) + 10px);
+    top: var(--pos);
+  }
+  
+  .thumb-label-below  .v-slider-thumb__label::before {
+    bottom: calc(var(--v-slider-thumb-size) + 4px);
+    transform: rotate(180deg);
+}
+  
 
 }
 </style>
+./utils
