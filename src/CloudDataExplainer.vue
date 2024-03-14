f0fa45b2292d8415c7d5366c03fbbdea14a6c331
John Arban Lewis <johnarban@users.noreply.github.com>
2024-03-14 16:10:42 -0400

    update statistic text for graphs

--- src/CloudDataExplainer.vue
+++ src/CloudDataExplainer.vue
@@ -137,7 +137,21 @@
                 When you choose a specific location on the map, the mean and median of the cloud cover amounts are displayed for that location over the selected range of years. You can also display cloud cover values for a single year at a time.
               </p>
             </details>
-          
+            
+            
+            <details class="faq">
+              <summary><v-icon>mdi-chart-bar</v-icon> What is in the bar chart on the left?</summary>
+              <p>
+                The left hand graph is called a histogram. For your selected location, it tells you how many years since 2003 were Clear, Partly Cloudy, Mostly Cloudy, or Cloudy at this location. If you have filtered the data by El Nino, Neutral, or La Nina years, your chosen subset will be colored with the blue to white gradient, while the rest of the years will appear gray.
+              </p>
+            </details>
+
+            <details class="faq">
+              <summary><v-icon>mdi-chart-scatter-plot</v-icon> What is in the graph on the right?</summary>
+              <p>
+                The right hand graph is a scatter plot showing the cloud cover for your chosen location for each year since 2003. If you have filtered the data by El Nino, Neutral, or La Nina years, or for a single year, those data points will be displayed in yellow. The background of the graph shows what cloud cover values correspond to the weather conditions of Clear, Partly Cloudy, etc.
+              </p>
+            </details>
           </v-window-item>
           
           <v-window-item value="weather">
